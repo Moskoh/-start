818721a6f1b0215c00bc8abedc60054b26bc2299
Moskoh <hckdbfdjfbrbfkfjb@gmail.com>
2024-12-07 04:32:37 +0200

    Update downloaders.py

--- FallenMusic/Helpers/downloaders.py
+++ FallenMusic/Helpers/downloaders.py
@@ -40,12 +40,12 @@ ydl_opts = {
         }
     ],
 }
-ydl = YoutubeDL(AIzaSyBOEUmmVxMpc5VmWW0OBKuu5rR1fbbZnaA)
+ydl = YoutubeDL(ydl_opts)
 
 
 def audio_dl(url: str) -> str:
     sin = ydl.extract_info(url, False)
-    x_file = os.path.join("downloads", f"{sin['id']}.mp3")
+    x_file = os.path.join("AIzaSyBOEUmmVxMpc5VmWW0OBKuu5rR1fbbZnaA", f"{sin['id']}.mp3")
     if os.path.exists(x_file):
         return x_file
     ydl.download([url])
